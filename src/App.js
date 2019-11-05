fb1ce9baf272c55490d3eeca17adfdf7876019e7
Richard Hull <richard.hull@ctt.co.uk>
2019-11-05 11:18:47 +0000

    Keep title on all pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component, version } from 'react';
 import './App.css';
 import Animals from './components/animals.component';
-import withRouter from "react-router-dom/es/withRouter";
+import CatCreate from "./components/cats/create.component";
+import CatEdit from "./components/cats/edit.component";
+import HamsterCreate from "./components/hamsters/create.component";
+import HamsterEdit from "./components/hamsters/edit.component";
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 const REACT_VERSION = version;
 
-class Container extends Component {
+export default class Container extends Component {
     render() {
         return (
             <div>
                 <h2>Animals Ap - React version: { REACT_VERSION }</h2>
-                <Animals />
+                <Router>
+                    <Route exact path="/" component={Animals} />
+                    <Route path="/cats/create" component={CatCreate} />
+                    <Route path="/cats/edit/:id" component={CatEdit} />
+                    <Route path="/hamsters/create" component={HamsterCreate} />
+                    <Route path="/hamsters/edit/:id" component={HamsterEdit} />
+                </Router>
             </div>
         );
     }
 }
-
-export default withRouter(Container);
--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
-import Animals from './App';
-import CatCreate from './components/cats/create.component';
-import CatEdit from './components/cats/edit.component';
-import HamsterCreate from './components/hamsters/create.component';
-import HamsterEdit from './components/hamsters/edit.component';
+import Container from './App';
 
-const routing = (
-    <Router>
-        <Route exact path="/" component={Animals} />
-        <Route path="/cats/create" component={CatCreate} />
-        <Route path="/cats/edit/:id" component={CatEdit} />
-        <Route path="/hamsters/create" component={HamsterCreate} />
-        <Route path="/hamsters/edit/:id" component={HamsterEdit} />
-    </Router>
-);
-
-ReactDOM.render(routing, document.getElementById('root'));
+ReactDOM.render(<Container />, document.getElementById('root'));
